32e2223aa1630a236a202e26e9cfe39b2760203d
Isa Milefchik <isavmilefchik@gmail.com>
2019-05-23 01:20:10 -0700

    Removed unnecessary comments

--- post_upscale_model.py
+++ post_upscale_model.py
@@ -14,13 +14,6 @@ class Post_Upscale_Model(nn.Module):
         self.sigm2 = nn.Sigmoid()
         self.upscale = nn.ConvTranspose1d(1,1,int(WINDOW_SIZE/2)+1)
         # self.upscale = nn.ConvTranspose1d(1,1,2,stride=2)
-        
-        # super(SRCNN,self).__init__()
-        # self.conv1 = nn.Conv2d(3,64,kernel_size=9,padding=4);
-        # self.relu1 = nn.ReLU();
-        # self.conv2 = nn.Conv2d(64,32,kernel_size=1,padding=0);
-        # self.relu2 = nn.ReLU();
-        # self.conv3 = nn.Conv2d(32,3,kernel_size=5,padding=2);
 
     def forward(self, x):
         out = self.conv1(x)
